c9bd330e11bc6f852ef571284627f0684476636b
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-02 01:21:04 -0300

    feat: base class for handlers

---
+++ Messages/CommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA.CommonLib.Messages
+{
+    public abstract class CommandHandler
+    {
+        protected CommandHandler() => ValidationResult = new ValidationResult();
+        protected ValidationResult ValidationResult { get; set; }
+        protected void AddError(string mensagem) => ValidationResult.Errors.Add(new ValidationFailure(string.Empty, mensagem));
+    }
+}
