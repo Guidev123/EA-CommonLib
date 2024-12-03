e06cf78e17e3afb9ecd1decc16cc905b9bb4a60f
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-02 23:08:02 -0300

    feat: delete de usuarios

---
+++ MessageBus/Integration/DeleteCustomer/DeleteCustomerIntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA.CommonLib.MessageBus.Integration.DeleteCustomer
+{
+    public class DeleteCustomerIntegrationEvent : IntegrationEvent
+    {
+        public DeleteCustomerIntegrationEvent(Guid id)
+        {
+            Id = id;
+            AggregateId = id;
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
