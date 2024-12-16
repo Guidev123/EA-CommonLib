2b86dab31987f1347f97da759ed651004e8db98b
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-16 19:30:56 -0300

    feat: specifications

---
+++ Specs/Validation/Rule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA.CommonLib.Specs.Validation
+{
+    public class Rule<T>(Specification<T> spec, string errorMessage)
+    {
+        private readonly Specification<T> _specificationSpec = spec;
+
+        public string ErrorMessage { get; } = errorMessage;
+
+        public bool Validate(T obj) => _specificationSpec.IsSatisfiedBy(obj);
+    }
+}
