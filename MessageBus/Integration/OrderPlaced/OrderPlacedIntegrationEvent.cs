c0496bc2d1479a8cd7eca080138ef2b1fc402cdb
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-17 07:23:00 -0300

    feat

---
+++ MessageBus/Integration/OrderPlaced/OrderPlacedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace EA.CommonLib.MessageBus.Integration.OrderPlaced
+{
+    public class OrderPlacedIntegrationEvent : IntegrationEvent
+    {
+        public OrderPlacedIntegrationEvent(Guid customerId)
+        {
+            AggregateId = customerId;
+            CustomerId = customerId;
+        }
+        public Guid CustomerId { get; private set; }
+    }
+}
